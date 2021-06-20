1d1e6beea1f0368c6359cf901114976b0246a2fa
Samuel Fernandes <samueldiablo73@gmail.com>
2021-06-20 12:17:43 -0300

    Projeto api para TD-CARD

---
+++ core/views.py
@@ -0,0 +1,44 @@
+
+from rest_framework.response import Response
+from core.models import Post
+from core.serializers import PostSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+from core.serializers import UserSerializer
+from rest_framework import permissions
+from core.permissions import IsOwnerOrReadOnly
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+
+
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'posts': reverse('post-list', request=request, format=format)
+    })
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsOwnerOrReadOnly]
+
+
